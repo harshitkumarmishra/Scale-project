56ec6d8463bebed7d14b79224912b9c5944368da
harshitkumarmishra <44098310+harshitkumarmishra@users.noreply.github.com>
2021-12-01 10:41:21 +0530

    Update sram_traffic_rs.py

--- sram_traffic_rs.py
+++ sram_traffic_rs.py
@@ -1,1 +1,67 @@
+import math 
+from tqdm import tqdm
 
+neg_inf = -1 * math.pow(2,32)
+
+def sram_traffic(
+        dimension_rows=4,
+        dimension_cols=4,
+        ifmap_h=7, ifmap_w=7,
+        filt_h=3, filt_w=3,
+        num_channels=3,
+        strides=1, num_filt=8,
+        ofmap_base=2000000, filt_base=1000000, ifmap_base=0,
+        sram_read_trace_file="sram_read.csv",
+        sram_write_trace_file="sram_write.csv"
+):
+
+
+    # Dimensions of output feature map channel
+    E_h = (ifmap_h - filt_h + strides) / strides
+    E_w = (ifmap_w - filt_w + strides) / strides
+    
+    # Number of pixels in one convolution window
+    px_per_conv_window = filt_h * filt_w * num_channels
+    r2c = px_per_conv_window
+
+    # Total number of ofmap px across all channels
+    num_ofmap_px = E_h * E_w * num_filt
+    e2  = E_h * E_w
+    e2m = num_ofmap_px
+    
+    # Variables to calculate folds in runtime
+    num_h_fold = math.ceil(e2/dimension_rows)
+    num_v_fold = math.ceil(num_filt/dimension_cols)
+
+    cycles = 0
+
+    read_cycles, util = gen_read_trace(
+                            cycle = cycles,
+                            dim_rows = dimension_rows,
+                            dim_cols = dimension_cols,
+                            num_v_fold = int(num_v_fold),
+                            num_h_fold = int(num_h_fold),
+                            ifmap_h = ifmap_h, ifmap_w= ifmap_w,
+                            filt_h= filt_h, filt_w= filt_w,
+                            num_channels= num_channels, stride=strides,
+                            ofmap_h= int(E_h), ofmap_w= int(E_w), num_filters = num_filt,
+                            filt_base= filt_base, ifmap_base= ifmap_base,
+                            sram_read_trace_file= sram_read_trace_file
+                            )
+
+    write_cycles = gen_write_trace(
+                        cycle = cycles,
+                        dim_rows = dimension_rows,
+                        dim_cols = dimension_cols,
+                        #num_v_fold = int(num_v_fold),
+                        #num_h_fold = int(num_h_fold),
+                        ofmap_h = int(E_h), ofmap_w = int(E_w),
+                        num_filters = num_filt,
+                        ofmap_base = ofmap_base,
+                        conv_window_size = r2c,
+                        sram_write_trace_file = sram_write_trace_file
+                        )
+
+    cycles = max(read_cycles, write_cycles)
+    str_cycles = str(cycles)
+    return(str_cycles, util)
